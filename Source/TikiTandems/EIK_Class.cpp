047934f7bd07fc6c2aa94175d7368f0c799338b8
Simon Zafirovski <zafirovskisimon@gmail.com>
2024-01-22 01:52:46 +0100

    AchievmentCreated

---
+++ Source/TikiTandems/EIK_Class.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EIK_Class.h"
+
+EIK_Class::EIK_Class()
+{
+}
+
+EIK_Class::~EIK_Class()
+{
+}
---
+++ Source/TikiTandems/EIK_Class.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class TIKITANDEMS_API EIK_Class
+{
+public:
+	EIK_Class();
+	~EIK_Class();
+};
